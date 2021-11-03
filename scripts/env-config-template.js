baef5134e27f52072ef6bd944c756f5f511c5325
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-11-03 14:40:55 +0100

    add appdi information to local.env.template add env-config-template.js to create the needed env-config.js for the frontend add run locally frontend to the scripts folder

--- scripts/env-config-template.js
+++ scripts/env-config-template.js
@@ -1,8 +1,8 @@
 // used as 'environment' variables, 
 // Single WebPage
 // App ID
-window.VUE_APPID_CLIENT_ID='b3adeb3b-36fc-40cb-9bc3-dd6f15047195'
-window.VUE_APPID_DISCOVERYENDPOINT='https://us-south.appid.cloud.ibm.com/oauth/v4/a7ec8ce4-3602-42c7-8e88-6f8a9db31935/.well-known/openid-configuration'
+window.VUE_APPID_CLIENT_ID='APPID_CLIENT_ID_TEMPLATE'
+window.VUE_APPID_DISCOVERYENDPOINT='APPID_DISCOVERYENDPOINT_TEMPLATE'
 // Application URLs
 window.VUE_APP_API_URL_PRODUCTS="http://localhost:8081/category"
 window.VUE_APP_API_URL_CATEGORIES="http://localhost:8081/category"
