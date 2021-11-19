3994a7fec07d0abba61fdf7ebc8f393fac840a26
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-11-19 15:37:32 +0100

    fix run-locally-service-catalog

--- scripts/run-locally-service-catalog.sh
+++ scripts/run-locally-service-catalog.sh
@@ -8,48 +8,38 @@ function _out() {
   echo "$(date +'%F %H:%M:%S') $@"
 }
 
-function triggerScript() { 
+function triggerScript() {
 
-  echo "Have you created a Postgres instance?"
+  echo "1. Have you created an App ID instance?"
+  echo "Copy the credentials in local.env: APPID_CLIENT_ID, APPID_AUTH_SERVER_URL"
+  echo ""
+  echo "2. Have you created a Postgres instance?"
   echo "Copy the credentials in local.env: POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_URL, POSTGRES_CERTIFICATE_FILE_NAME"
   echo "Copy the Postgres certificate in code/service-catalog/src/main/resources/certificates"
-  echo "Starting catalog service locally in a container ..."
+  echo "Starting catalog service locally ..."
   echo curl  \"http://localhost:8081/category\"
   echo curl  \"http://localhost:8081/category/2/products\"
 
-  cd ${root_folder}
   CFG_FILE=${root_folder}/local.env
   if [ ! -f $CFG_FILE ]; then
     _out Config file local.env is missing!
     exit 1
   fi
-
+  
   set -o allexport
   source $CFG_FILE
-
-  POSTGRES_URL=$(echo $POSTGRES_URL| cut -d'?' -f 1)
-  CERTIFICATE_PATH=/cloud-postgres-cert
-  POSTGRES_URL="$POSTGRES_URL?sslmode=verify-full&sslrootcert=$CERTIFICATE_PATH"
+   
   APPID_AUTH_SERVER_URL=${APPID_AUTH_SERVER_URL}
   APPID_CLIENT_ID=${APPID_CLIENT_ID}
 
-  POSTGRES_CERTIFICATE_DATA=$(<${root_folder}/code/service-catalog/src/main/resources/certificates/${POSTGRES_CERTIFICATE_FILE_NAME})
+  POSTGRES_URL=$(echo $POSTGRES_URL| cut -d'?' -f 1)
+  CERTIFICATE_PATH=${root_folder}/code/service-catalog/src/main/resources/certificates/cloud-postgres-cert
+  cp ${root_folder}/code/service-catalog/src/main/resources/certificates/$POSTGRES_CERTIFICATE_FILE_NAME $CERTIFICATE_PATH
+  POSTGRES_URL="$POSTGRES_URL?sslmode=verify-full&sslrootcert=$CERTIFICATE_PATH"
 
   cd ${root_folder}/code/service-catalog
-  podman container stop service-catalog --ignore
-  podman container rm -f service-catalog --ignore
-  podman build --file Dockerfile --tag service-catalog
-
-  podman run --name=service-catalog \
-    -it \
-    -e POSTGRES_CERTIFICATE_DATA="${POSTGRES_CERTIFICATE_DATA}" \
-    -e POSTGRES_USERNAME="${POSTGRES_USERNAME}" \
-    -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" \
-    -e POSTGRES_URL="${POSTGRES_URL}" \
-    -e APPID_AUTH_SERVER_URL="${APPID_AUTH_SERVER_URL}" \
-    -e APPID_CLIENT_ID="${APPID_CLIENT_ID}" \
-    -p 8081:8081/tcp \
-    localhost/service-catalog:latest
+  mvn clean package
+  mvn quarkus:dev
 }
 
 triggerScript
