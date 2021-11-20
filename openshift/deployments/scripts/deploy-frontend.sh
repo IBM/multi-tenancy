91a6f17755ae86cb5df6c31ed282b114d6db021a
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-11-20 19:55:00 +0100

    update configs and build config

--- openshift/deployments/scripts/deploy-frontend.sh
+++ openshift/deployments/scripts/deploy-frontend.sh
@@ -2,10 +2,20 @@
 
 # **************** Global variables
 root_folder=$(cd $(dirname $0); cd ../../../; pwd)
-APPID_SECRETS_TEMPLATE_FILE="appid-secrets-template.yaml"
-POSTGRES_SECRETS_TEMPLATE_FILE="postgres-secrets-template.yaml"
-APPID_SECRETS_FILE="appid-secrets.yaml"
-POSTGRES_SECRETS_FILE="postgres-secrets.yaml"
+# secrets
+export APPID_SECRETS_TEMPLATE_FILE="appid-secrets-template.yaml"
+export POSTGRES_SECRETS_TEMPLATE_FILE="postgres-secrets-template.yaml"
+export APPID_SECRETS_FILE="appid-secrets.yaml"
+export POSTGRES_SECRETS_FILE="postgres-secrets.yaml"
+# configmap
+export FRONTEND_TEMPLATE_FILE="configmap-frontend-template.yaml"
+export FRONTEND_FILE="configmap-frontend.yaml"
+export URL_PRODUCTS="xxx"
+export URL_ORDERS="xxx"
+export URL_CATEGORIES="xxx"
+export APP_CATEGORY_NAME="Movies"
+export APP_HEADLINE="Frontend OpenShift"
+export APP_ROOT="'/'"
 
 # **************** Load environments variables
 
@@ -53,22 +63,31 @@ function createFrontendSecrets () {
 }
 
 function createFrontendConfigMap () {
-  
-  echo "-> create app id secrets"
-  TMP_FILE_1=tmp_frontend_secrect_1.yaml
-  TMP_FILE_2=tmp_frontend_secrect_2.yaml
+ 
+  echo "-> create configmap for frontend"
+  TMP_FILE_1=tmp_frontend_config_1.yaml
+  TMP_FILE_2=tmp_frontend_config_2.yaml
 
-  KEY_TO_REPLACE=APPID_CLIENT_ID
-  sed "s+$KEY_TO_REPLACE+$APPID_CLIENT_ID+g" "${root_folder}/openshift/deployments/secrets/$APPID_SECRETS_TEMPLATE_FILE" > ${root_folder}/openshift/deployments/secrets/$TMP_FILE_1
+  KEY_TO_REPLACE=URL_PRODUCTS_1
+  sed "s+$KEY_TO_REPLACE+$URL_PRODUCTS+g" "${root_folder}/openshift/deployments/configmaps/$FRONTEND_TEMPLATE_FILE" > ${root_folder}/openshift/deployments/configmaps/$TMP_FILE_1
 
-  KEY_TO_REPLACE=APPID_DISCOVERYENDPOINT
-  sed "s+$KEY_TO_REPLACE+$APPID_DISCOVERYENDPOINT+g" "${root_folder}/openshift/deployments/secrets/$TMP_FILE_1" > ${root_folder}/openshift/deployments/secrets/$TMP_FILE_2
+  KEY_TO_REPLACE=URL_ORDERS_1
+  sed "s+$KEY_TO_REPLACE+$URL_ORDERS+g" "${root_folder}/openshift/deployments/configmaps/$TMP_FILE_1" > ${root_folder}/openshift/deployments/configmaps/$TMP_FILE_2
 
-  KEY_TO_REPLACE=APPID_AUTH_SERVER_URL
-  sed "s+$KEY_TO_REPLACE+$APPID_AUTH_SERVER_URL+g" "${root_folder}/openshift/deployments/secrets/$TMP_FILE_2" > ${root_folder}/openshift/deployments/secrets/$APPID_SECRETS_FILE
+  KEY_TO_REPLACE=URL_CATEGORIES_1
+  sed "s+$KEY_TO_REPLACE+$URL_CATEGORIES+g" "${root_folder}/openshift/deployments/configmaps/$TMP_FILE_2" > ${root_folder}/openshift/deployments/configmaps/$TMP_FILE_1
 
-  rm -f ${root_folder}/openshift/deployments/secrets/$TMP_FILE_1
-  rm -f ${root_folder}/openshift/deployments/secrets/$TMP_FILE_2
+  KEY_TO_REPLACE=APP_CATEGORY_NAME_1
+  sed "s+$KEY_TO_REPLACE+$APP_CATEGORY_NAME+g" "${root_folder}/openshift/deployments/configmaps/$TMP_FILE_1" > ${root_folder}/openshift/deployments/configmaps/$TMP_FILE_2
+
+  KEY_TO_REPLACE=APP_HEADLINE_1
+  sed "s+$KEY_TO_REPLACE+$APP_HEADLINE+g" "${root_folder}/openshift/deployments/configmaps/$TMP_FILE_2" > ${root_folder}/openshift/deployments/configmaps/$TMP_FILE_1
+
+  KEY_TO_REPLACE=APP_ROOT_1
+  sed "s+$KEY_TO_REPLACE+$APP_ROOT+g" "${root_folder}/openshift/deployments/configmaps/$TMP_FILE_1" > ${root_folder}/openshift/deployments/configmaps/$FRONTEND_FILE
+
+  rm -f ${root_folder}/openshift/deployments/configmaps/$TMP_FILE_1
+  rm -f ${root_folder}/openshift/deployments/configmaps/$TMP_FILE_2
 }
 
 
@@ -77,3 +96,5 @@ function createFrontendConfigMap () {
 # **********************************************************************************
 
 createFrontendSecrets
+
+createFrontendConfigMap
