2e1e0697fa4d9a11054b4d327b756e49dccae189
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-01-05 13:44:32 +0100

    update code

--- code/frontend/public/env-config.js
+++ code/frontend/public/env-config.js
@@ -1,8 +1,8 @@
 // used as 'environment' variables, 
 // Single WebPage
 // App ID
-window.VUE_APPID_CLIENT_ID='b3adeb3b-36fc-40cb-9bc3-dd6f15047195'
-window.VUE_APPID_DISCOVERYENDPOINT='https://us-south.appid.cloud.ibm.com/oauth/v4/a7ec8ce4-3602-42c7-8e88-6f8a9db31935/.well-known/openid-configuration'
+window.VUE_APPID_CLIENT_ID='b12a05c3-8164-45d9-a1b8-af1dedf8ccc3'
+window.VUE_APPID_DISCOVERYENDPOINT='https://eu-de.appid.cloud.ibm.com/oauth/v4/e1b4e68e-f1ea-44b2-b8f3-eed95fa21c13/.well-known/openid-configuration'
 // Application URLs
 window.VUE_APP_API_URL_PRODUCTS="http://localhost:8081/category"
 window.VUE_APP_API_URL_CATEGORIES="http://localhost:8081/category"
