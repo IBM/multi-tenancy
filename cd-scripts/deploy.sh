b481a758dcec76ebebebb78638a3244ed43f7b67
Adam de Leeuw <deleeuw@uk.ibm.com>
2022-01-20 13:27:51 +0000

    remove git-token on frontend too

--- cd-scripts/deploy.sh
+++ cd-scripts/deploy.sh
@@ -259,8 +259,8 @@ if [[ "${ARTIFACT_URL}" == *"github"* ]]; then
   #http_response=$(curl -H "Authorization: token ${GIT_TOKEN}" -s -w "%{http_code}\n" ${ARTIFACT_URL} -o $DEPLOYMENT_FILE)    
   http_response=$(curl -s -w "%{http_code}\n" ${ARTIFACT_URL} -o $DEPLOYMENT_FILE)    
 else
-  #http_response=$(curl -H "PRIVATE-TOKEN: ${GIT_TOKEN}" -s -w "%{http_code}\n" ${ARTIFACT_URL} -o $DEPLOYMENT_FILE)
-  http_response=$(curl -s -w "%{http_code}\n" ${ARTIFACT_URL} -o $DEPLOYMENT_FILE)
+  http_response=$(curl -H "PRIVATE-TOKEN: ${GIT_TOKEN}" -s -w "%{http_code}\n" ${ARTIFACT_URL} -o $DEPLOYMENT_FILE)
+  #http_response=$(curl -s -w "%{http_code}\n" ${ARTIFACT_URL} -o $DEPLOYMENT_FILE)
 fi
 if [ "$http_response" != "200" ]; then
   echo "Failed to download the artifact. Please provide the correct token as the env property 'artifact-token'."
@@ -429,7 +429,8 @@ echo "${ARTIFACT_URL}"
 echo ${ARTIFACT_URL}
 
 if [[ "${ARTIFACT_URL}" == *"github"* ]]; then
-  http_response=$(curl -H "Authorization: token ${GIT_TOKEN}" -s -w "%{http_code}\n" ${ARTIFACT_URL} -o $DEPLOYMENT_FILE)    
+  #http_response=$(curl -H "Authorization: token ${GIT_TOKEN}" -s -w "%{http_code}\n" ${ARTIFACT_URL} -o $DEPLOYMENT_FILE)    
+  http_response=$(curl -s -w "%{http_code}\n" ${ARTIFACT_URL} -o $DEPLOYMENT_FILE)    
 else
   http_response=$(curl -H "PRIVATE-TOKEN: ${GIT_TOKEN}" -s -w "%{http_code}\n" ${ARTIFACT_URL} -o $DEPLOYMENT_FILE)
 fi
